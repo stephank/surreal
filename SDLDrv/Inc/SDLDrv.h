51ca19673f3ba11cfdf67503079594aaac5a4e06
Ste´phan Kochen <stephan@kochen.nl>
2012-05-06 13:16:27 +0200

    Start of SDL2 support in SDLDrv. UNTESTED.
    This just compiles, and some advanced functionality it missing (console
    commands, TryRenderDevice.)

--- SDLDrv/Inc/SDLDrv.h
+++ SDLDrv/Inc/SDLDrv.h
@@ -49,14 +49,17 @@ class USDLClient : public UClient, public FNotifyHook
 	DECLARE_CLASS(USDLClient,UClient,CLASS_Transient|CLASS_Config,SDLDrv)
 
 	// Configuration.
-	BITFIELD			StartupFullscreen;
-	BITFIELD			SlowVideoBuffering;
+	BITFIELD	StartupFullscreen;
+	BITFIELD	SlowVideoBuffering;
 
 	// Variables.
-	UBOOL				InMenuLoop;
-	UBOOL				ConfigReturnFullscreen;
-	INT				NormalMouseInfo[3];
-	INT				CaptureMouseInfo[3];
+	UBOOL		InMenuLoop;
+	UBOOL		ConfigReturnFullscreen;
+	INT			NormalMouseInfo[3];
+	INT			CaptureMouseInfo[3];
+	UBOOL		LastKey;
+	FTime		RepeatTimer;
+	EInputKey	KeysymMap[65536];
 
 	// Constructors.
 	USDLClient();
@@ -85,16 +88,6 @@ class USDLClient : public UClient, public FNotifyHook
 -----------------------------------------------------------------------------*/
 
 //
-// Viewport window status.
-//
-enum ESDLViewportStatus
-{
-	SDL_ViewportOpening	= 0, // Viewport is opening.
-	SDL_ViewportNormal	= 1, // Viewport is operating normally.
-	SDL_ViewportClosing	= 2, // Viewport is closing and CloseViewport has been called.
-};
-
-//
 // A SDL viewport.
 //
 class USDLViewport : public UViewport
@@ -103,36 +96,20 @@ class USDLViewport : public UViewport
 	DECLARE_WITHIN(USDLClient)
 
 	// Variables.
-	ESDLViewportStatus		ViewportStatus;
+	SDL_Window*		Window;
+	SDL_Renderer*	Renderer;
+	UClass*			RenDevOverride;
+	INT				CurrentColorBytes;
 	INT				HoldCount;
-	DWORD				BlitFlags;
-	UBOOL				Borderless;
-	UBOOL				RestoreAutoRepeat;
-	UBOOL				LastKey;
-	FTime				RepeatTimer;
-	UBOOL				Mapped;
-	UBOOL				Iconified;
-	
-	// Mouse.
-	//INT				MouseAccel_N;
-	//INT				MouseAccel_D;
-	//INT				MouseThreshold;
+	DWORD			BlitFlags;
 
-	// Info saved during captures and fullscreen sessions.
-	INT				SavedColorBytes;
-	INT				SavedCaps;
+	// Mouse motion events.
+	// These variables are only used by USDLClient.
+	INT				MouseX;
+	INT				MouseY;
+	INT				MouseWheelY;
 
 	// SDL Keysym to EInputKey map.
-	BYTE				KeysymMap[65536];
-
-	// ShiftMask map.
-	BYTE				ShiftMaskMap[256];
-
-	// KeyRepeat map.
-	BYTE				KeyRepeatMap[256];
-
-	// Map of keys that can be WM_CHAR'd
-	BYTE				WMCharMap[256];
 	
 	// Constructor.
 	USDLViewport();
@@ -142,11 +119,8 @@ class USDLViewport : public UViewport
 	void ShutdownAfterError();
 
 	// UViewport interface.
-	UBOOL Lock( FPlane FlashScale, FPlane FlashFog, FPlane ScreenClear, DWORD RenderLockFlags, BYTE* HitData=NULL, INT* HitSize=0 );
-	UBOOL Exec( const TCHAR* Cmd, FOutputDevice& Ar );
 	UBOOL ResizeViewport( DWORD BlitFlags, INT NewX=INDEX_NONE, INT NewY=INDEX_NONE, INT NewColorBytes=INDEX_NONE );
 	UBOOL IsFullscreen();
-	void Unlock( UBOOL Blit );
 	void Repaint( UBOOL Blit );
 	void SetModeCursor();
 	void UpdateWindowFrame();
@@ -156,19 +130,6 @@ class USDLViewport : public UViewport
 	void* GetWindow();
 	void* GetServer();
 	void SetMouseCapture( UBOOL Capture, UBOOL Clip, UBOOL FocusOnly );
-
-	// UXViewport interface.
-	void ToggleFullscreen();
-	void EndFullscreen();
-	void FindAvailableModes();
-	void TryHardware3D( USDLViewport* Viewport );
-	UBOOL CauseInputEvent( INT iKey, EInputAction Action, FLOAT Delta=0.0 );
-	void TryRenderDevice( const TCHAR* ClassName, INT NewX, INT NewY, INT NewColorBytes, UBOOL Fullscreen );
-	void Tick();
-	void Iconify();
-	//	Cursor GetNullCursor();
-	void CaptureInputs();
-	void ReleaseInputs();
 };
 
 #endif //_INC_SDLDRV
--- SDLDrv/Src/SDLClient.cpp
+++ SDLDrv/Src/SDLClient.cpp
@@ -28,6 +28,95 @@ USDLClient::USDLClient()
 {
 	guard(USDLClient::USDLClient);
 
+	// Zero out Keysym map.
+	for (INT i=0; i<65536; i++)
+		KeysymMap[i] = IK_None;
+
+	// Remap important keys.
+	KeysymMap[SDL_SCANCODE_BACKSPACE]= IK_Backspace;
+	KeysymMap[SDL_SCANCODE_TAB]		= IK_Tab;
+	KeysymMap[SDL_SCANCODE_RETURN]	= IK_Enter;
+	KeysymMap[SDL_SCANCODE_ESCAPE]	= IK_Escape;
+	KeysymMap[SDL_SCANCODE_SPACE]	= IK_Space;
+
+	// Special remaps.
+	KeysymMap[SDL_SCANCODE_GRAVE]	= IK_Tilde;
+
+	// The following are all the SDL keycodes that don't generate characters,
+	// that we can map to something in the engine.
+	KeysymMap[SDL_SCANCODE_CAPSLOCK]= IK_CapsLock;
+
+	KeysymMap[SDL_SCANCODE_F1]		= IK_F1;
+	KeysymMap[SDL_SCANCODE_F2]		= IK_F2;
+	KeysymMap[SDL_SCANCODE_F3]		= IK_F3;
+	KeysymMap[SDL_SCANCODE_F4]		= IK_F4;
+	KeysymMap[SDL_SCANCODE_F5]		= IK_F5;
+	KeysymMap[SDL_SCANCODE_F6]		= IK_F6;
+	KeysymMap[SDL_SCANCODE_F7]		= IK_F7;
+	KeysymMap[SDL_SCANCODE_F8]		= IK_F8;
+	KeysymMap[SDL_SCANCODE_F9]		= IK_F9;
+	KeysymMap[SDL_SCANCODE_F10]		= IK_F10;
+	KeysymMap[SDL_SCANCODE_F11]		= IK_F11;
+	KeysymMap[SDL_SCANCODE_F12]		= IK_F12;
+
+	KeysymMap[SDL_SCANCODE_PRINTSCREEN]= IK_PrintScrn;
+	KeysymMap[SDL_SCANCODE_SCROLLLOCK]= IK_ScrollLock;
+	KeysymMap[SDL_SCANCODE_PAUSE]	= IK_Pause;
+	KeysymMap[SDL_SCANCODE_INSERT]  = IK_Insert;
+	KeysymMap[SDL_SCANCODE_HOME]	= IK_Home;
+	KeysymMap[SDL_SCANCODE_PAGEUP]	= IK_PageUp;
+	KeysymMap[SDL_SCANCODE_DELETE]	= IK_Delete;
+	KeysymMap[SDL_SCANCODE_END]		= IK_End;
+	KeysymMap[SDL_SCANCODE_PAGEDOWN]= IK_PageDown;
+	KeysymMap[SDL_SCANCODE_RIGHT]	= IK_Right;
+	KeysymMap[SDL_SCANCODE_LEFT]	= IK_Left;
+	KeysymMap[SDL_SCANCODE_DOWN]	= IK_Down;
+	KeysymMap[SDL_SCANCODE_UP]		= IK_Up;
+
+	KeysymMap[SDL_SCANCODE_NUMLOCKCLEAR]= IK_NumLock;
+	KeysymMap[SDL_SCANCODE_KP_DIVIDE]= IK_GreySlash;
+	KeysymMap[SDL_SCANCODE_KP_MULTIPLY]= IK_GreyStar;
+	KeysymMap[SDL_SCANCODE_KP_MINUS]= IK_GreyMinus;
+	KeysymMap[SDL_SCANCODE_KP_PLUS]	= IK_GreyPlus;
+	KeysymMap[SDL_SCANCODE_KP_ENTER]= IK_Enter;
+	KeysymMap[SDL_SCANCODE_KP_1]	= IK_NumPad1;
+	KeysymMap[SDL_SCANCODE_KP_2]	= IK_NumPad2;
+	KeysymMap[SDL_SCANCODE_KP_3]	= IK_NumPad3;
+	KeysymMap[SDL_SCANCODE_KP_4]	= IK_NumPad4;
+	KeysymMap[SDL_SCANCODE_KP_5]	= IK_NumPad5;
+	KeysymMap[SDL_SCANCODE_KP_6]	= IK_NumPad6;
+	KeysymMap[SDL_SCANCODE_KP_7]	= IK_NumPad7;
+	KeysymMap[SDL_SCANCODE_KP_8]	= IK_NumPad8;
+	KeysymMap[SDL_SCANCODE_KP_9]	= IK_NumPad9;
+	KeysymMap[SDL_SCANCODE_KP_0]	= IK_NumPad0;
+	KeysymMap[SDL_SCANCODE_KP_PERIOD]= IK_NumPadPeriod;
+
+	KeysymMap[SDL_SCANCODE_F13]		= IK_F13;
+	KeysymMap[SDL_SCANCODE_F14]		= IK_F14;
+	KeysymMap[SDL_SCANCODE_F15]		= IK_F15;
+	KeysymMap[SDL_SCANCODE_F16]		= IK_F16;
+	KeysymMap[SDL_SCANCODE_F17]		= IK_F17;
+	KeysymMap[SDL_SCANCODE_F18]		= IK_F18;
+	KeysymMap[SDL_SCANCODE_F19]		= IK_F19;
+	KeysymMap[SDL_SCANCODE_F20]		= IK_F20;
+	KeysymMap[SDL_SCANCODE_F21]		= IK_F21;
+	KeysymMap[SDL_SCANCODE_F22]		= IK_F22;
+	KeysymMap[SDL_SCANCODE_F23]		= IK_F23;
+	KeysymMap[SDL_SCANCODE_F24]		= IK_F24;
+	KeysymMap[SDL_SCANCODE_EXECUTE]	= IK_Execute;
+	KeysymMap[SDL_SCANCODE_HELP]	= IK_Help;
+	KeysymMap[SDL_SCANCODE_SELECT]	= IK_Select;
+
+	KeysymMap[SDL_SCANCODE_CANCEL]	= IK_Cancel;
+	KeysymMap[SDL_SCANCODE_SEPARATOR]= IK_Separator;
+
+	KeysymMap[SDL_SCANCODE_LCTRL]	= IK_LControl;
+	KeysymMap[SDL_SCANCODE_LSHIFT]	= IK_LShift;
+	KeysymMap[SDL_SCANCODE_LALT]	= IK_Alt;
+	KeysymMap[SDL_SCANCODE_RCTRL]	= IK_RControl;
+	KeysymMap[SDL_SCANCODE_RSHIFT]	= IK_RShift;
+	KeysymMap[SDL_SCANCODE_RALT]	= IK_Alt;
+
 	unguard;
 }
 
@@ -60,8 +149,8 @@ void USDLClient::Init( UEngine* InEngine )
 	// Default res option.
 	if( ParseParam(appCmdLine(),TEXT("defaultres")) )
 	{
-		WindowedViewportX  = FullscreenViewportX  = 640;
-		WindowedViewportY  = FullscreenViewportY  = 480;
+		WindowedViewportX = FullscreenViewportX = 640;
+		WindowedViewportY = FullscreenViewportY = 480;
 	}
 
 	// Success.
@@ -78,7 +167,9 @@ void USDLClient::Destroy()
 
 	SDL_Quit();
 	debugf( NAME_Exit, TEXT("SDL client shut down.") );
+
 	Super::Destroy();
+
 	unguard;
 }
 
@@ -90,20 +181,13 @@ void USDLClient::ShutdownAfterError()
 {
 	debugf( NAME_Exit, TEXT("Executing USDLClient::ShutdownAfterError") );
 
-	// End mouse capture.
-
 	// Kill the audio subsystem.
 	if( Engine && Engine->Audio )
-	{
 		Engine->Audio->ConditionalShutdownAfterError();
-	}
 
 	// Release all viewports.
 	for( INT i=Viewports.Num()-1; i>=0; i-- )
-	{
-		USDLViewport* Viewport = (USDLViewport*)Viewports( i );
-		Viewport->ConditionalShutdownAfterError();
-	}
+		Viewports( i )->ConditionalShutdownAfterError();
 
 	Super::ShutdownAfterError();
 }
@@ -120,13 +204,13 @@ void USDLClient::NotifyDestroy( void* Src )
 //
 UBOOL USDLClient::Exec( const TCHAR* Cmd, FOutputDevice& Ar )
 {
-	guard(USDLClient::Exec);
-	if( UClient::Exec( Cmd, Ar ) )
-	{
-		return 1;
-	}
-	return 0;
-	unguard;
+       guard(USDLClient::Exec);
+       if( UClient::Exec( Cmd, Ar ) )
+       {
+               return 1;
+       }
+       return 0;
+       unguard;
 }
 
 //
@@ -138,34 +222,163 @@ void USDLClient::Tick()
 {
 	guard(USDLClient::Tick);
 	INT i;
+	USDLViewport* Viewport;
+	EInputKey Key, InputKey;
+	EInputAction Action;
 
-	// Tick the viewports.
-	for( i=0; i<Viewports.Num(); i++ )
+	SDL_Event Event;
+	while( SDL_PollEvent( &Event ) )
 	{
-		USDLViewport* Viewport = CastChecked<USDLViewport>(Viewports(i));
-		Viewport->Tick();
+		switch( Event.type )
+		{
+		case SDL_KEYDOWN:
+		case SDL_KEYUP:
+
+			// Find the viewport.
+			for( i=0; i<Viewports.Num(); i++ ){
+				Viewport = CastChecked<USDLViewport>(Viewports(i));
+				if( SDL_GetWindowID(Viewport->Window) == Event.key.windowID )
+					break;
+			}
+			if( i == Viewports.Num() )
+				break;
+
+			// Determine the action.
+			if( Event.key.state == SDL_PRESSED )
+				Action = Event.key.repeat ? IST_Hold : IST_Press;
+			else
+				Action = IST_Release;
+
+			// See if it's a scancode we've mapped.
+			if( (Key = KeysymMap[Event.key.keysym.scancode]) != IK_None ){
+				InputKey = Key;
+			}
+			else{
+				// Try to handle as a basic ASCII value.
+				Sint32 Keysym = Event.key.keysym.sym;
+				Key = (EInputKey) Keysym;
+
+				// Always send upper case letters to the input system.
+				// The engine NEVER processes lowercase letters,
+				// since lowercase keycodes are mapped to the F* keys.
+				if( Keysym >= 'a' && Keysym <= 'z' )
+					InputKey = (EInputKey) (Keysym - 32);
+				else
+					InputKey = (EInputKey) Keysym;
+			}
+
+			// Send key to input system.
+			if ( Key > IK_None && Key < IK_MAX )
+				Engine->InputEvent( Viewport, InputKey, Action );
+
+			// Send to text processor
+			Engine->Key( Viewport, Key );
+
+			break;
+
+		case SDL_MOUSEBUTTONDOWN:
+		case SDL_MOUSEBUTTONUP:
+
+			// Find the viewport.
+			for( i=0; i<Viewports.Num(); i++ ){
+				Viewport = CastChecked<USDLViewport>(Viewports(i));
+				if( SDL_GetWindowID(Viewport->Window) == Event.button.windowID )
+					break;
+			}
+			if( i == Viewports.Num() )
+				break;
+
+			// Determine the action.
+			if( Event.button.state == SDL_PRESSED )
+				Action = IST_Press;
+			else
+				Action = IST_Release;
+
+			// Map the button to the engine key.
+			switch (Event.button.button)
+			{
+				case SDL_BUTTON_LEFT:
+					InputKey = IK_LeftMouse;
+					break;
+				case SDL_BUTTON_MIDDLE:
+					InputKey = IK_MiddleMouse;
+					break;
+				case SDL_BUTTON_RIGHT:
+					InputKey = IK_RightMouse;
+					break;
+			}
+
+			// Send to input system.
+			Engine->InputEvent( Viewport, InputKey, Action );
+
+			break;
+
+		case SDL_MOUSEWHEEL:
+
+			// Find the viewport.
+			for( i=0; i<Viewports.Num(); i++ ){
+				Viewport = CastChecked<USDLViewport>(Viewports(i));
+				if( SDL_GetWindowID(Viewport->Window) == Event.wheel.windowID )
+					break;
+			}
+			if( i == Viewports.Num() )
+				break;
+
+			// Regardless of amount scrolled, we emit just one mouse wheel event
+			// to the engine per tick. Collect the total scroll here.
+			Viewport->MouseWheelY += Event.wheel.y;
+
+		case SDL_MOUSEMOTION:
+
+			// Find the viewport.
+			for( i=0; i<Viewports.Num(); i++ ){
+				Viewport = CastChecked<USDLViewport>(Viewports(i));
+				if( SDL_GetWindowID(Viewport->Window) == Event.motion.windowID )
+					break;
+			}
+			if( i == Viewports.Num() )
+				break;
+
+			// Collect data for a combined mouse motion event to the engine.
+			// FIXME: How to send absolute coordinates when not grabbed?
+			Viewport->MouseX += Event.motion.xrel;
+			Viewport->MouseY += Event.motion.yrel;
+
+			break;
+
+		default:
+			break;
+		}
 	}
 
-	// Blit any viewports that need blitting.
 	USDLViewport* BestViewport = NULL;
 	for( i=0; i<Viewports.Num(); i++ )
 	{
-		USDLViewport* Viewport = CastChecked<USDLViewport>(Viewports(i));
-		check(!Viewport->HoldCount);
-		/*		
-		//dv: FIXME???
-		if( !(Viewport->XWindow) )
-		{
-			// Window was closed via close button.
-			delete Viewport;
-			return;
+		Viewport = CastChecked<USDLViewport>(Viewports(i));
+
+		// Flush collected motion events.
+		INT Delta;
+		if( (Delta = Viewport->MouseWheelY)!=0 ) {
+			Viewport->MouseWheelY = 0;
+			if( Delta > 0 )
+				Engine->InputEvent( Viewport, IK_MouseWheelUp, IST_Press );
+			else
+				Engine->InputEvent( Viewport, IK_MouseWheelDown, IST_Press );
 		}
-		*/
-		//else
+		if( (Delta = Viewport->MouseX)!=0 ){
+			Viewport->MouseX = 0;
+			Engine->InputEvent( Viewport, IK_MouseX, IST_Axis, +Delta );
+		}
+		if( (Delta = Viewport->MouseY)!=0 ){
+			Viewport->MouseY = 0;
+			Engine->InputEvent( Viewport, IK_MouseY, IST_Axis, -Delta );
+		}
+
+		// Blit any viewports that need blitting.
+		check(!Viewport->HoldCount);
 		if
 		(	Viewport->IsRealtime()
-		&&	Viewport->SizeX
-		&&	Viewport->SizeY
+		&&	Viewport->RenDev
 		&&	(!BestViewport || Viewport->LastUpdateTime<BestViewport->LastUpdateTime) )
 		{
 			BestViewport = Viewport;
@@ -188,24 +401,11 @@ UViewport* USDLClient::NewViewport( const FName Name )
 }
 
 //
-// Configuration change.
-//
-void USDLClient::PostEditChange()
-{
-	guard(USDLClient::PostEditChange);
-
-	Super::PostEditChange();
-
-	unguard;
-}
-
-//
 // Enable or disable all viewport windows that have ShowFlags set (or all if ShowFlags=0).
 //
 void USDLClient::EnableViewportWindows( DWORD ShowFlags, int DoEnable )
 {
 	guard(USDLClient::EnableViewportWindows);
-
 	unguard;
 }
 
@@ -214,14 +414,11 @@ void USDLClient::EnableViewportWindows( DWORD ShowFlags, int DoEnable )
 //
 void USDLClient::ShowViewportWindows( DWORD ShowFlags, int DoShow )
 {
-	guard(USDLClient::ShowViewportWindows); 	
+	guard(USDLClient::ShowViewportWindows);
 	for( int i=0; i<Viewports.Num(); i++ )
 	{
-		/* FIXME
 		USDLViewport* Viewport = (USDLViewport*)Viewports(i);
-		if( (Viewport->Actor->ShowFlags & ShowFlags)==ShowFlags )
-			XMapWindow(XDisplay, Viewport->XWindow);
-		*/
+		SDL_ShowWindow( Viewport->Window );
 	}
 	unguard;
 }
